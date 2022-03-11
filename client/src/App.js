ec3b5bc700cbbc1833a56ece1621713369f5d7b6
Nuccino92 <anthnucci@hotmail.com>
2022-03-10 19:47:36 -0500

    added browser router with paths to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import Application from "./components/application/Application";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 function App() {
-  return <div className="App">hi</div>;
+  const [auth, setAuth] = useState(false);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        {auth ? (
+          <Routes>
+            <Route path="/" element={<Application />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        )}
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
