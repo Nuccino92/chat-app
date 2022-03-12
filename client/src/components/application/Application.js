23ec29242a89abef2025b598d9c41732a13077e1
Nuccino92 <anthnucci@hotmail.com>
2022-03-12 17:57:08 -0500

    the main component

--- client/src/components/application/Application.js
+++ client/src/components/application/Application.js
@@ -1,7 +1,18 @@
 import "./Application.css";
+import ChatWindow from "./chatwindow/ChatWindow";
+import Nav from "./nav/Nav";
+import Sidenav from "./sidenav/Sidenav";
 
 const Application = () => {
-  return <div>this is the app</div>;
+  return (
+    <div className="Application">
+      <Nav></Nav>
+      <div className="Application-body">
+        <Sidenav />
+        <ChatWindow />
+      </div>
+    </div>
+  );
 };
 
 export default Application;
