20ca378a1a355ffac6595ac139486cb995ebd330
Nuccino92 <anthnucci@hotmail.com>
2022-03-13 20:59:33 -0400

    added state for each component

--- client/src/components/application/Application.js
+++ client/src/components/application/Application.js
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import "./Application.css";
 import ChatWindow from "./chatwindow/ChatWindow";
+import Contacts from "./contacts/Contacts";
 import Nav from "./nav/Nav";
+import Profile from "./profile/Profile";
 import Sidenav from "./sidenav/Sidenav";
 
 const Application = () => {
+  const [tab, setTab] = useState("home");
+
   return (
     <div className="Application">
-      <Nav></Nav>
-      <div className="Application-body">
-        <Sidenav />
-        <ChatWindow />
-      </div>
+      {tab === "home" && (
+        <>
+          {" "}
+          <Nav setTab={setTab}></Nav>
+          <div className="Application-body">
+            <Sidenav />
+            <ChatWindow />
+          </div>
+        </>
+      )}
+      {tab === "contacts" && <Contacts setTab={setTab} />}
+      {tab === "profile" && <Profile setTab={setTab} />}
     </div>
   );
 };
