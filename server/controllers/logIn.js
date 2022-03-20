7f917eac5728a29adf3043425508974f805bb928
Nuccino92 <anthnucci@hotmail.com>
2022-03-19 23:02:55 -0400

    added passport auth to login controller

--- server/controllers/logIn.js
+++ server/controllers/logIn.js
@@ -1,5 +1,54 @@
-const logIn_POST = async (req, res) => {
-  res.send("here");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+
+const { User } = require("../models");
+
+const logIn_POST = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  passport.authenticate("local", { session: false }, (err, user, info) => {
+    if (!email) {
+      return res.status(401).json({
+        msg: "Please enter an email",
+        param: "email",
+      });
+    }
+    if (!password) {
+      return res.status(401).json({
+        message: "Please enter password",
+        param: "password",
+      });
+    }
+    if (err) {
+      return res.status(401).json({
+        message: err.message,
+      });
+    }
+    if (!user) {
+      return res.status(401).json({
+        message: "You entered the incorrect information",
+        user: user,
+        param: null,
+      });
+    }
+
+    req.login(user, { session: false }, (err) => {
+      jwt.sign(
+        {
+          id: user.id,
+        },
+        process.env.JWT_SECRET,
+        // 24 hours
+        { expiresIn: 86400 },
+        async (err, token) => {
+          if (err) throw err;
+          await User.findByPk(user.id).then((user) => {
+            res.status(201).json({ token, user });
+          });
+        }
+      );
+    });
+  })(req, res, next);
 };
 
 module.exports = {
