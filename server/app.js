29bfad354c5962ac40cac5026058ad0de33ba26b
Nuccino92 <anthnucci@hotmail.com>
2022-03-19 23:03:36 -0400

    configured passport

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const passport = require("passport");
 
 const { sequelize } = require("./models");
+require("./config/passport.js");
 
 const registerRoutes = require("./routes/register.js");
 const contactRoutes = require("./routes/contacts.js");
@@ -12,12 +14,15 @@ const messageRoutes = require("./routes/messages.js");
 
 const app = express();
 
+app.use(passport.initialize());
+
 dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, async () => {
   console.log(`server connected on ${PORT}`);
   await sequelize.authenticate();
