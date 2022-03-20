440c4e2f72e45495824164da57db924d840d90aa
Nuccino92 <anthnucci@hotmail.com>
2022-03-19 23:01:01 -0400

    passed express validator function through register route

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { register_POST } = require("../controllers/register.js");
+const registerValidation = require("../validation/register.js");
 
 const router = express.Router();
 
-router.post("/", register_POST);
+router.post("/", registerValidation, register_POST);
 
 module.exports = router;
