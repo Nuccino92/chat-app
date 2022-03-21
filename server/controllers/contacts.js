ff5273ab9bd76ad723facbb94e7311301cf0801d
Nuccino92 <anthnucci@hotmail.com>
2022-03-20 20:20:03 -0400

    all controller methods for contacts

--- server/controllers/contacts.js
+++ server/controllers/contacts.js
@@ -1,15 +1,51 @@
-const { sequelize, Contact } = require("../models");
+const { Contact } = require("../models");
+const { Op } = require("sequelize");
 
 const allContacts_GET = async (req, res) => {
-  res.send("got all contacts");
+  const { id } = req.params;
+  await Contact.findAll({
+    where: {
+      [Op.or]: [{ userID1: id }, { userID2: id }],
+    },
+  })
+    .then((contacts) => {
+      return res.status(201).json(contacts);
+    })
+    .catch((err) => {
+      return res.status(500).json(err);
+    });
 };
 
 const confirmedContacts_GET = async (req, res) => {
-  res.send("got all accepted contacts ");
+  const { id } = req.params;
+  await Contact.findAll({
+    where: {
+      confirmed: 1,
+      [Op.or]: [{ userID1: id }, { userID2: id }],
+    },
+  })
+    .then((contacts) => {
+      return res.status(201).json(contacts);
+    })
+    .catch((err) => {
+      return res.status(500).json(err);
+    });
 };
 
 const pendingContact_GET = async (req, res) => {
-  res.send("got all pending contacts");
+  const { id } = req.params;
+  await Contact.findAll({
+    where: {
+      confirmed: 0,
+      [Op.or]: [{ userID1: id }, { userID2: id }],
+    },
+  })
+    .then((contacts) => {
+      return res.status(201).json(contacts);
+    })
+    .catch((err) => {
+      return res.status(500).json(err);
+    });
 };
 
 const createContact_POST = async (req, res) => {
@@ -31,18 +67,49 @@ const createContact_POST = async (req, res) => {
     });
 };
 
-const acceptContact_POST = async (req, res) => {
-  res.send("accepted");
+const acceptContact_PUT = async (req, res) => {
+  const { id } = req.params;
+  const { user2 } = req.body;
+
+  await Contact.findOne({
+    where: {
+      [Op.or]: [{ userID1: id }, { userID2: id }],
+      [Op.or]: [{ userID1: user2 }, { userID2: user2 }],
+    },
+  })
+    .then(async (contact) => {
+      await contact.update({ confirmed: 1 });
+      await contact.save();
+      return res.status(201).json(contact);
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: "Oops! Unable to update user" });
+    });
 };
 
-const declineContact_POST = async (req, res) => {
-  res.send("declined");
+const declineContact_DELETE = async (req, res) => {
+  const { id } = req.params;
+  const { user2 } = req.body;
+
+  await Contact.findOne({
+    where: {
+      [Op.or]: [{ userID1: id }, { userID2: id }],
+      [Op.or]: [{ userID1: user2 }, { userID2: user2 }],
+    },
+  })
+    .then(async (contact) => {
+      await contact.destroy();
+      return res.status(201).json({ msg: "user deleted" });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: "Oops! Unable to update user" });
+    });
 };
 
 module.exports = {
   createContact_POST,
-  acceptContact_POST,
-  declineContact_POST,
+  acceptContact_PUT,
+  declineContact_DELETE,
   allContacts_GET,
   confirmedContacts_GET,
   pendingContact_GET,
