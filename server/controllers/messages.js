c1d2aa8846be951d0c9087460e5a13fc7508bc72
Nuccino92 <anthnucci@hotmail.com>
2022-03-20 20:19:28 -0400

    create message

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,5 +1,19 @@
+const { Messages } = require("../models");
+
 const createMessage_POST = async (req, res) => {
-  res.send("message created");
+  const { conversationID, senderID, content } = req.body;
+
+  await Messages.create({
+    conversationID,
+    senderID,
+    content,
+  })
+    .then((message) => {
+      return res.status(201).json(message);
+    })
+    .catch((err) => {
+      return res.status(500).json(err);
+    });
 };
 
 module.exports = {
