3b9f0f704a06fac71eb8c3aca19ceddc27d33efe
Nuccino92 <anthnucci@hotmail.com>
2022-03-22 22:48:25 -0400

    added multer middleware

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const passport = require("passport");
+const multer = require("multer");
 
 const { updateUser_PUT, authUser_GET } = require("../controllers/users.js");
 
@@ -7,7 +8,23 @@ const router = express.Router();
 
 const auth = passport.authenticate("jwt", { session: false });
 
-router.get("/auth/:id", auth, authUser_GET);
-router.put("/:id", updateUser_PUT);
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: fileStorageEngine,
+  limits: {
+    fileSize: 1024 * 1024 * 10,
+  },
+});
+
+router.get("/auth", auth, authUser_GET);
+router.put("/:id", upload.single("profilePicture"), updateUser_PUT);
 
 module.exports = router;
