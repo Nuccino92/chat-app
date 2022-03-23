43c791d7f6a243b5c67ef7db411d02279584b044
Nuccino92 <anthnucci@hotmail.com>
2022-03-22 22:52:22 -0400

    component to display all input errors

--- client/src/components/formError/FormError.js
+++ client/src/components/formError/FormError.js
@@ -1,7 +1,22 @@
 import "./FormError.css";
 
 const FormError = ({ message, location }) => {
-  return <div className="FormError">{message}</div>;
+  return (
+    <div
+      style={
+        location === "register"
+          ? { marginBottom: "59px" }
+          : location === "logIn"
+          ? { marginBottom: "186px" }
+          : location === "contact"
+          ? { position: "relative", margin: "10px 0 0" }
+          : null
+      }
+      className="FormError"
+    >
+      {message}
+    </div>
+  );
 };
 
 export default FormError;
