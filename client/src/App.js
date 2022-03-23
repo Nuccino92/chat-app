54f71b1f301a0eba52e23b2abe22e906338662f5
Nuccino92 <anthnucci@hotmail.com>
2022-03-22 22:47:09 -0400

    now checking the auth from redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useState } from "react";
+import { useEffect } from "react";
 import Application from "./components/application/Application";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
+import { useDispatch, useSelector } from "react-redux";
+import { loadUser } from "./redux/actions/user";
 
 function App() {
-  const [auth, setAuth] = useState(false);
+  const dispatch = useDispatch();
+  const { isAuthenticated } = useSelector((state) => state.userReducer);
+
+  useEffect(() => {
+    dispatch(loadUser());
+  }, [dispatch]);
 
   return (
     <BrowserRouter>
       <div className="App">
-        {auth ? (
+        {isAuthenticated ? (
           <Routes>
             <Route path="/" element={<Application />} />
+            <Route path="/*" element={<Application />} />
           </Routes>
         ) : (
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/*" element={<Login />} />
           </Routes>
         )}
       </div>
