6703a74fe0d62b51f1736494d44c75b9860deb7d
Nuccino92 <anthnucci@hotmail.com>
2022-03-22 22:52:44 -0400

    added methods to successfully update profile

--- client/src/components/application/profile/Profile.js
+++ client/src/components/application/profile/Profile.js
@@ -1,13 +1,71 @@
 import "./Profile.css";
 import { useSpring, animated } from "react-spring";
 import { MdOutlineClose } from "react-icons/md";
+import { useDispatch, useSelector } from "react-redux";
+import { loadUser, logOutUser } from "../../../redux/actions/user";
+import { useState } from "react";
+import { updateUserRequest } from "../../../api/users";
+import convertBase64 from "../../../utils/base64";
 
 const Profile = ({ setTab }) => {
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.userReducer);
+
+  // state that holds profilePicture to be used in update photo, created this state to show photo that is being uploaded for sent to backend using base64
+  const [uploadProfilePic, setUploadProfilePic] = useState(user.profilePicture);
+
+  const [profileData, setProfileData] = useState({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    profilePicture: user.profilePicture,
+  });
+
   const style = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
     config: { duration: 1000 },
   });
+
+  const handleChange = async (e) => {
+    const { name, value, type, files } = e.target;
+
+    // state will show chosen photo using base 64 function
+    if (type === "file") {
+      await convertBase64(files[0]).then((res) => {
+        setUploadProfilePic(res);
+      });
+    }
+
+    setProfileData((prev) => {
+      return {
+        ...prev,
+        [name]: type === "file" ? files[0] : value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData();
+
+    data.append("firstName", profileData.firstName);
+    data.append("lastName", profileData.lastName);
+    data.append("profilePicture", profileData.profilePicture);
+
+    await updateUserRequest(user.id, data)
+      .then(() => {
+        dispatch(loadUser());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleLogout = () => {
+    dispatch(logOutUser());
+  };
+
   return (
     <div className="Profile">
       <animated.div style={style}>
@@ -20,35 +78,50 @@ const Profile = ({ setTab }) => {
           />
         </header>
         <div className="Profile-body">
-          <h2>Anthony Nucci</h2>
-          <img
-            src="https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg"
-            alt="Profile"
-          />
+          <h2>
+            {user.firstName}&#160;
+            {user.lastName}
+          </h2>
+          <img src={`${user.profilePicture}`} alt="Profile" />
           <h3>User Id</h3>
-          <p>#353454745664</p>
+          <p>{user.id}</p>
           <ul className="edit-profile">
             <li className="edit-name">
               <p>First name</p>
-              <input type="text" defaultValue={"Anthony"} />
+              <input
+                onChange={handleChange}
+                name="firstName"
+                type="text"
+                defaultValue={`${user.firstName}`}
+              />
               <p>Last name</p>
-              <input type="text" defaultValue={"Nucci"} />
+              <input
+                onChange={handleChange}
+                name="lastName"
+                type="text"
+                defaultValue={`${user.lastName}`}
+              />
             </li>
             <li className="edit-photo">
               <label htmlFor="picture">
-                {" "}
                 Update Photo
-                <img
-                  src="https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg"
-                  alt="Profile"
-                />
-                <input id="picture" name="profilePicture" type="file"></input>
+                <img src={uploadProfilePic} alt="Profile" />
+                <input
+                  onChange={handleChange}
+                  name="profilePicture"
+                  id="picture"
+                  type="file"
+                ></input>
               </label>
             </li>
-            <button>Edit Profile</button>
+            <button onClick={handleSubmit}>Edit Profile</button>
           </ul>
 
-          <button title="log out" className="log-out-button">
+          <button
+            onClick={handleLogout}
+            title="log out"
+            className="log-out-button"
+          >
             Log Out
           </button>
         </div>
