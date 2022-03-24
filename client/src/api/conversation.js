d8aa7396a7be1433b6a33bbd24be3cc1f9cab6b2
Nuccino92 <anthnucci@hotmail.com>
2022-03-23 22:53:11 -0400

    added get user conversation call

--- client/src/api/conversation.js
+++ client/src/api/conversation.js
@@ -2,6 +2,15 @@ import axios from "axios";
 
 const url = "http://localhost:8000/conversation/";
 
+export const getUserConversations = (id) =>
+  axios({
+    method: "get",
+    url: url + "all/" + id,
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+
 //GET
 export const getConversationRequest = () => {
   //PARAMS (USER1, USER2) 2 PARAMS
