fb7b988fa70b5c17734bccc0899a0517bee4f1f2
Nuccino92 <anthnucci@hotmail.com>
2022-03-24 22:47:58 -0400

    refactor

--- client/src/components/application/sidenav/friendrequests/FriendRequestsCard.js
+++ client/src/components/application/sidenav/friendrequests/FriendRequestsCard.js
@@ -1,36 +1,41 @@
 import { useEffect, useState } from "react";
 import { FcCheckmark } from "react-icons/fc";
 import { IoMdClose } from "react-icons/io";
+import { useSelector } from "react-redux";
 import {
   acceptContactRequest,
   deleteContactRequest,
 } from "../../../../api/contact";
+import { startConversationRequest } from "../../../../api/conversation";
 import { getUserRequest } from "../../../../api/users";
 
 const FriendRequestsCard = ({ request }) => {
+  const { userID1, userID2, id } = request;
+
   const [userInfo, setUserInfo] = useState(null);
   const [loading, setLoading] = useState(true);
   const [chosen, setChosen] = useState(false);
 
   useEffect(() => {
     const getUser = async () => {
-      await getUserRequest(request.userID1).then((res) => {
+      await getUserRequest(userID1).then((res) => {
         setUserInfo(res.data);
         setLoading(false);
       });
     };
     setLoading(true);
     getUser();
-  }, [request.userID1]);
+  }, [userID1]);
 
   const handleAccept = async () => {
-    await acceptContactRequest(request.id).then(() => {
+    await acceptContactRequest(id).then(async () => {
+      await startConversationRequest(userID1, userID2);
       setChosen(true);
     });
   };
 
   const handleDecline = async () => {
-    await deleteContactRequest(request.id).then(() => {
+    await deleteContactRequest(id).then(() => {
       setChosen(true);
     });
   };
