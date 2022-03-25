c0fd7ac23c4fe977c37f80e7537239c219ec63ed
Nuccino92 <anthnucci@hotmail.com>
2022-03-24 22:45:43 -0400

    added delete route

--- server/routes/conversation.js
+++ server/routes/conversation.js
@@ -3,14 +3,16 @@ const {
   getConversation_GET,
   startConversation_POST,
   getConversationMessages_GET,
-  getAllUserConversations,
+  getAllUserConversations_GET,
+  deleteConversation_DELETE,
 } = require("../controllers/conversation.js");
 
 const router = express.Router();
 
-router.get("/all/:id", getAllUserConversations);
+router.get("/all/:id", getAllUserConversations_GET);
 router.get("/find/:user1/:user2", getConversation_GET);
 router.post("/start/:id", startConversation_POST);
 router.get("/messages/:conversationID", getConversationMessages_GET);
+router.delete("/delete/:user1/:user2", deleteConversation_DELETE);
 
 module.exports = router;
