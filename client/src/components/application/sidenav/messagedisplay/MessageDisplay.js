e397578883f03797c2aef5214342abfb530dd505
Nuccino92 <anthnucci@hotmail.com>
2022-03-25 21:50:34 -0400

    moved logic to sidenav

--- client/src/components/application/sidenav/messagedisplay/MessageDisplay.js
+++ client/src/components/application/sidenav/messagedisplay/MessageDisplay.js
@@ -1,46 +1,16 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { getUserConversations } from "../../../../api/conversation";
 import "./MessageDisplay.css";
 import MessageDisplayCard from "./MessageDisplayCard";
 
-const MessageDisplay = () => {
-  const { user } = useSelector((state) => state.userReducer);
-
-  const [receivedConversations, setReceivedConversations] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const getConversations = async () => {
-      await getUserConversations(user.id).then((res) => {
-        setReceivedConversations(res.data);
-        setLoading(false);
-      });
-    };
-    getConversations();
-  }, [user.id]);
-
+const MessageDisplay = ({ filteredData }) => {
   return (
-    <>
-      {loading ? (
-        <></>
-      ) : (
-        <div className="MessageDisplay">
-          <h3>Chats</h3>
-          <div className="MessageDisplay-body">
-            {receivedConversations.map((conversation, index) => {
-              return (
-                <MessageDisplayCard
-                  id={user.id}
-                  conversation={conversation}
-                  key={index}
-                />
-              );
-            })}
-          </div>
-        </div>
-      )}
-    </>
+    <div className="MessageDisplay">
+      <h3>Chats</h3>
+      <div className="MessageDisplay-body">
+        {filteredData.map((contact, index) => {
+          return <MessageDisplayCard contact={contact} key={index} />;
+        })}
+      </div>
+    </div>
   );
 };
 
--- client/src/components/application/sidenav/messagedisplay/MessageDisplayCard.js
+++ client/src/components/application/sidenav/messagedisplay/MessageDisplayCard.js
@@ -1,54 +1,27 @@
-import { useEffect, useState } from "react";
-import { getUserRequest } from "../../../../api/users";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getConversation } from "../../../../redux/actions/chat";
 import { GET_PARTICIPANT } from "../../../../redux/actions/types";
 
-const MessageDisplayCard = ({ conversation, id }) => {
+const MessageDisplayCard = ({ contact }) => {
   const dispatch = useDispatch();
 
-  const { userID1, userID2 } = conversation;
-
-  const [user2Info, setUser2Info] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const { user } = useSelector((state) => state.userReducer);
 
   const handleClick = async () => {
-    dispatch(getConversation(userID1, userID2));
-    dispatch({ type: GET_PARTICIPANT, payload: user2Info });
+    dispatch(getConversation(contact.id, user.id));
+    dispatch({ type: GET_PARTICIPANT, payload: contact });
   };
 
-  useEffect(() => {
-    const getUser = async (id) => {
-      await getUserRequest(id).then((res) => {
-        setUser2Info(res.data);
-        setLoading(false);
-      });
-    };
-
-    if (userID1 !== id) {
-      getUser(userID1);
-    }
-    if (userID2 !== id) {
-      getUser(userID2);
-    }
-  }, [setLoading, setUser2Info, id, userID1, userID2]);
-
   return (
-    <>
-      {loading ? (
-        <></>
-      ) : (
-        <div className="MessageDisplayCard" onClick={handleClick}>
-          <img src={user2Info.profilePicture} alt="Contact" />
-          <div>
-            <h4>
-              {user2Info.firstName} {user2Info.lastName}
-            </h4>
-            <p>I NEED TO GET THE MESSAGES FOR THIS THING TO WORK</p>
-          </div>
-        </div>
-      )}
-    </>
+    <div className="MessageDisplayCard" onClick={handleClick}>
+      <img src={contact.profilePicture} alt="Contact" />
+      <div>
+        <h4>
+          {contact.firstName} {contact.lastName}
+        </h4>
+        <p>I NEED TO GET THE MESSAGES FOR THIS THING TO WORK</p>
+      </div>
+    </div>
   );
 };
 
