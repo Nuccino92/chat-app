bf359f5aa0d9d47ee6834d4a5a5cd0e6fa52518e
Nuccino92 <anthnucci@hotmail.com>
2022-03-25 21:53:10 -0400

    added scroll into view

--- client/src/components/application/chatwindow/ChatWindow.js
+++ client/src/components/application/chatwindow/ChatWindow.js
@@ -3,16 +3,28 @@ import ChatWindowMessage from "./chatWindowMessage/ChatWindowMessage";
 import Picker from "emoji-picker-react";
 import { BsEmojiWink } from "react-icons/bs";
 import { AiOutlineSend } from "react-icons/ai";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { getUserRequest } from "../../../api/users";
+import { useEffect, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createMessageRequest } from "../../../api/messages";
+import logo from "../../../images/chatify-logo.png";
+import { getConversation } from "../../../redux/actions/chat";
 
 const ChatWindow = () => {
+  const scrollRef = useRef();
+  const inputRef = useRef(null);
+  const dispatch = useDispatch();
+
   const { user } = useSelector((state) => state.userReducer);
-  const { messages } = useSelector((state) => state.chatReducer);
+  const { messages, chat } = useSelector((state) => state.chatReducer);
   const { participant } = useSelector((state) => state.chatReducer);
+
   const [message, setMessage] = useState("");
   const [emojiPicker, setEmojiPicker] = useState(false);
+  const [createMessageData, setCreateMessageData] = useState({
+    conversationID: null,
+    senderID: user.id,
+    content: "",
+  });
 
   const onEmojiClick = (event, emojiObject) => {
     setMessage((prevInput) => prevInput + emojiObject.emoji);
@@ -24,9 +36,54 @@ const ChatWindow = () => {
   };
 
   const handleChange = (e) => {
-    const { value } = e.target;
+    const { value, name } = e.target;
     setMessage(value);
+
+    setCreateMessageData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
   };
+
+  const handleSubmit = async () => {
+    if (!createMessageData.content) return;
+    await createMessageRequest(createMessageData).then(() => {
+      dispatch(getConversation(chat.userID1, chat.userID2));
+    });
+    setCreateMessageData((prev) => {
+      return {
+        ...prev,
+        content: "",
+      };
+    });
+    setMessage("");
+  };
+
+  // keep content fully updated if last character was emoji in content
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({
+      behavior: "smooth",
+    });
+
+    setCreateMessageData((prev) => {
+      return {
+        ...prev,
+        content: message,
+      };
+    });
+  }, [message]);
+
+  // when selecting chat, make conversationID = chat.id
+  useEffect(() => {
+    setCreateMessageData((prev) => {
+      return {
+        ...prev,
+        conversationID: chat !== null ? chat.id : null,
+      };
+    });
+  }, [chat]);
 
   return (
     <div className="ChatWindow">
@@ -37,16 +94,33 @@ const ChatWindow = () => {
           <img src={participant.profilePicture} alt="User"></img>
         ) : null}
       </header>
-      <div className="ChatWindow-body">
+      <div
+        className="ChatWindow-body"
+        style={chat === null ? { background: "#1A3268" } : null}
+      >
+        {chat === null && (
+          <div className="no-chat-text">
+            <p>Select a chat</p>
+            <img src={logo} alt="Logo" />
+          </div>
+        )}
+
         {messages.map((info, index) => {
           let myMessage;
           info.senderID === user.id ? (myMessage = true) : (myMessage = false);
+
           return (
-            <ChatWindowMessage myMessage={myMessage} info={info} key={index} />
+            <div ref={scrollRef} key={index}>
+              <ChatWindowMessage
+                myMessage={myMessage}
+                info={info}
+                key={index}
+              />
+            </div>
           );
         })}
       </div>
-      <footer>
+      <footer style={chat === null ? { pointerEvents: "none" } : null}>
         <div className="send-message-container">
           {emojiPicker && (
             <Picker
@@ -60,13 +134,19 @@ const ChatWindow = () => {
             size={20}
           />
           <input
+            ref={inputRef}
+            name="content"
             onChange={handleChange}
             type="text"
             placeholder="Type a message"
             value={message}
           />
         </div>
-        <div className="send-message-button">
+        <div
+          style={chat === null ? { pointerEvents: "none" } : null}
+          className="send-message-button"
+          onClick={handleSubmit}
+        >
           <AiOutlineSend color="white" size={20} />
         </div>
       </footer>
