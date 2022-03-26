de47dcea906065e077afe71844e83ad4287e5a1f
Nuccino92 <anthnucci@hotmail.com>
2022-03-25 21:52:31 -0400

    moved api calls in here & added search filter

--- client/src/components/application/contacts/Contacts.js
+++ client/src/components/application/contacts/Contacts.js
@@ -3,16 +3,21 @@ import { useSpring, animated } from "react-spring";
 import { MdOutlineClose } from "react-icons/md";
 import Contact from "./contact/Contact";
 import { BiSearchAlt2 } from "react-icons/bi";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import AddContactModal from "./addcontactmodal/AddContactModal";
 import { useSelector } from "react-redux";
 import { getConfirmedContactsRequest } from "../../../api/contact";
+import { getUserRequest } from "../../../api/users";
 
 const Contacts = ({ setTab }) => {
+  const inputRef = useRef();
   const { user } = useSelector((state) => state.userReducer);
+
   const [addContactModal, setAddContactModal] = useState(false);
-  const [contacts, setContacts] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const [users, setUsers] = useState([]);
+  const [filteredData, setFitleredData] = useState([]);
 
   const style = useSpring({
     from: { opacity: 0 },
@@ -20,11 +25,37 @@ const Contacts = ({ setTab }) => {
     config: { duration: 1000 },
   });
 
+  const handleFilter = (e) => {
+    const word = e.target.value;
+
+    const newFilter = users.filter((user) => {
+      return (
+        user.firstName.toLowerCase().includes(word.toLowerCase()) ||
+        user.lastName.toLowerCase().includes(word.toLowerCase())
+      );
+    });
+
+    word === "" ? setFitleredData(users) : setFitleredData(newFilter);
+  };
+
   useEffect(() => {
     const getContacts = async () => {
       await getConfirmedContactsRequest(user.id).then((res) => {
-        setContacts(res.data);
-        setLoading(false);
+        res.data.forEach(async (e) => {
+          let id = e.userID1 !== user.id ? e.userID1 : e.userID2;
+
+          await getUserRequest(id).then((res) => {
+            setUsers((prev) => {
+              return [...prev, res.data];
+            });
+
+            setFitleredData((prev) => {
+              return [...prev, res.data];
+            });
+
+            setLoading(false);
+          });
+        });
       });
     };
     getContacts();
@@ -48,7 +79,12 @@ const Contacts = ({ setTab }) => {
               </button>
               <div className="Contacts-searchbar-container">
                 <BiSearchAlt2 size={20} color={"grey"} />
-                <input type="text" placeholder="search" />
+                <input
+                  ref={inputRef}
+                  type="text"
+                  placeholder="find contact"
+                  onChange={handleFilter}
+                />
               </div>
               <MdOutlineClose
                 style={{ cursor: "pointer" }}
@@ -57,7 +93,7 @@ const Contacts = ({ setTab }) => {
               />
             </header>
             <div className="Contacts-body">
-              {contacts.map((contact, index) => {
+              {filteredData.map((contact, index) => {
                 return (
                   <Contact setTab={setTab} contact={contact} key={index} />
                 );
