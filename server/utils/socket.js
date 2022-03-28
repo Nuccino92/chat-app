0ec16d903c8771e0c0a329de0ff9e9a97a726824
Nuccino92 <anthnucci@hotmail.com>
2022-03-27 21:13:29 -0400

    socket io util functions

---
+++ server/utils/socket.js
@@ -0,0 +1,21 @@
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+module.exports = {
+  addUser,
+  getUser,
+  removeUser,
+  users,
+};
